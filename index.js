1b50239cf684f8ec1f04bb84a52d6b031e6e22d0
A-N26 <108244197+A-N26@users.noreply.github.com>
2022-08-30 15:21:48 -0400

    commit

---
+++ index.js
@@ -0,0 +1,168 @@
+// ↓ To include and require package(s) for the application.
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { generateMarkdown } = require("./utils/generateMarkdown.js");
+
+// ↓ Array of questions to ask for creating the application. (Termed as objects in inquirer site documentation.)
+const questions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "Title",
+        message: "What is the title of the readme file? (required)",
+
+        validate: (TitleInput) => {
+          if (TitleInput) {
+            return true;
+          } else {
+            console.log("Please enter a title for the file.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Description",
+        message: "Add a description. (required)",
+
+        validate: (DescriptionInput) => {
+          if (DescriptionInput) {
+            return true;
+          } else {
+            console.log("Please enter a description.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "checkbox",
+        name: "Languages",
+        message:
+          "What languages did you use for this project? (check all that apply)",
+        choices: [
+          "Html",
+          "CSS",
+          "Javascript",
+          "Node.js",
+          "Bootstrap",
+          "jQuery",
+        ],
+
+        validate: (LanguagesCheck) => {
+          if (LanguagesCheck) {
+            return true;
+          } else {
+            console.log("Please check one or more that apply.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Installation",
+        message:
+          "What are the steps one needs to follow to install this application?",
+      },
+      {
+        type: "input",
+        name: "Usage",
+        message:
+          "Include usage instructions along with any images/media that may apply.",
+      },
+      {
+        type: "input",
+        name: "GUser",
+        message: "Please enter your GitHub Username. (required)",
+
+        validate: (GUserInput) => {
+          if (GUserInput) {
+            return true;
+          } else {
+            console.log("Please enter a valid GitHub username.");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "GLink",
+        message:
+          "Please enter a link to your github repository of this project. (required)",
+
+        validate: (LinkInput) => {
+          if (LinkInput) {
+            return true;
+          } else {
+            console.log("Please enter the link!");
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "DLink",
+        message: "Please enter a link to your deployed link of this project.",
+
+        validate: (LinkInput) => {
+          if (LinkInput) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      {
+        type: "input",
+        name: "Credits",
+        message:
+          "Were there any contributors, third-party assets, and/or tutorials that you would like to include?",
+      },
+      {
+        type: "list",
+        name: "Licence",
+        message: "Please specify the licence from the choices.",
+        choices: ["MIT", "ISC", "GNU GPLv3", "Unlicense", "none"],
+      },
+      {
+        type: "input",
+        name: "Badges",
+        message: "Would you like to include any badges?",
+      },
+    ])
+    .then((input) => {
+      return input;
+    });
+};
+
+// // ↓ To see the above questions (and type their answers/data) in terminal.
+// inquirer.prompt(questions).then((data) => {
+//   console.log(data);
+//   return data;
+// });
+
+// ↓ To create a function to write README file
+function writeToFile(filename, data) {
+  fs.writeFile(filename, data, (err) =>
+    err
+      ? console.error(err)
+      : console.log("file has been created and saved successfully!")
+  );
+}
+
+// ↓ To create a function to initialize app
+function init() {
+  questions()
+    .then((input) => {
+      return generateMarkdown(input);
+    })
+    .then((Markdown) => {
+      writeToFile(`./${filename}`, Markdown);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// ↓ Function call to initialize app.
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,95 @@
+// ↓ Function to return a licence badge based on which licence is passed in.
+// If there is no licence, return an empty string.
+function renderLicenceBadge(Licence) {
+  if (Licence === "none" && Licence !== "") {
+    return `[![License: Public Domain Mark 1.0](https://img.shields.io/badge/license-PublicDomain-blue.svg)](https://creativecommons.org/publicdomain/mark/1.0/)`;
+  } else if (Licence === "Unlicense") {
+    return `[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](https://choosealicense.com/licenses/unlicense/)`;
+  } else if (Licence === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://choosealicense.com/licenses/mit/)`;
+  } else if (Licence === "ISC") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
+  } else if (Licence === "GNU GPLv3") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://choosealicense.com/licenses/gpl-3.0/)`;
+  } else {
+    return "";
+  }
+}
+
+// ↓ Function to return the licence link.
+// If there is no licence, return un empty string.
+function renderLicenceLink(Licence) {
+  if (Licence === "unlicense") {
+    return `[Unlicense](https://choosealicense.com/licenses/unlicense/)`;
+  }
+  if (Licence === "MIT") {
+    return `[MIT](https://choosealicense.com/licenses/mit/)`;
+  }
+  if (Licence === "ISC") {
+    return `[ISC](https://opensource.org/licenses/ISC)`;
+  }
+  if (Licence === "GNU GPLv3") {
+    return `[GPL v3](https://choosealicense.com/licenses/gpl-3.0/)`;
+  } else {
+    return "";
+  }
+}
+
+// ↓ Function to create license section of README.
+//  If there is no licence, return un empty string.
+function renderLicenceSection(Licence) {
+  if (Licence !== "none") {
+    return `
+        ## License
+        ${renderLicenceBadge(Licence)}`;
+  } else {
+    return `
+        ## License
+        [No Licence has been set for this project](https://choosealicense.com/no-permission/)`;
+  }
+}
+
+// ↓ Function to generate markdown for README.
+function generateMarkdown(data) {
+  return `
+        # ${data.Title}
+
+        ## Description
+        ${data.Description}
+
+        ## Languages Used
+        ${data.Languages}
+
+        ## Table of Contents
+        * [Installation](#Installation)
+        * [Usage](#Usage)
+        * [Link](#Link)
+        * [Credits](#Credits)
+        * [Licence](#Licence)
+        * [Badges](#Badges)
+
+        ---
+
+        ## Installation
+        ${data.Installation}
+
+        ## Usage
+        ${data.Usage}
+
+        ## Link
+        ${data.GUser}
+        ${data.GLink}
+        ${data.DLink}
+
+        ## Credits
+        ${data.Credits}
+
+        ## License
+        ${renderLicenceSection(Licence)}
+
+        ## Badges
+        ${data.Badges}
+    `;
+}
+
+module.exports = { generateMarkdown };
